558dd547d78923552b628966d842b6135d7408db
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2025-07-29 16:51:14 +0300

    Remove example extension
    Add shutdown command for local development

---
+++ src/main/kotlin/io/github/smalauncher/mirrormaster/extensions/MirrorExtension.kt
@@ -0,0 +1,11 @@
+package io.github.smalauncher.mirrormaster.extensions
+
+import dev.kordex.core.extensions.Extension
+
+class MirrorExtension : Extension() {
+	override val name = "mirror"
+
+	override suspend fun setup() {
+		// TODO uh. everything.
+	}
+}
